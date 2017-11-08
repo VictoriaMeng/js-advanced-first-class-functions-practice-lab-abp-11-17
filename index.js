4d2e9e56519355f3918b51e4ccbfaa80535b9827
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 23:14:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  console.log(`${drivers[name]}`);
+  console.log(`${drivers.name}`);
 }
 
 logDriverNames({ name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 })
+
+
