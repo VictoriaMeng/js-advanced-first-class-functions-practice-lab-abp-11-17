411e38b0cdcb1cf5fe96a711bce4ebf21f6113e7
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 01:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 function logDriverNames (drivers) {
-
-  const callback = function (driver, index, drivers) {
-    console.log(driver.name);
+  for (const driver in drivers) {
+    
   }
 
-  drivers.forEach(callback);
 }
 
 function logDriversByHometown (drivers, place) {
 
-  const callback = function (driver, index, drivers) {
-    if (driver.hometown === place) {
-     console.log(driver.name);
-   }
-  }
 
-  drivers.forEach(callback);
 }
