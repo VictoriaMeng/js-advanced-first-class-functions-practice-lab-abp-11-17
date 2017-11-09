368d878b4d39fed4d3bdf8ca83363126de653d85
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 03:24:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function driversByName (drivers) {
 
 function totalRevenue (drivers) {
   const addRevenue = function(agg, index, driver, drivers) {
-    console.log(agg);
+    console.log(agg, driver.revenue);
     return agg + driver.revenue;
 
   };
