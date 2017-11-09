bd3b371b7e30c19718e32464b80190eec7c88a12
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 01:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const callback = function(driver) {
-  console.log(`${name}`);
-}// Code your solution in this file!
 
 
 function logDriverNames(drivers) {
-
+  for (const driver of drivers) {
+    
+  };
 }
