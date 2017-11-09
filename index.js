e8cfe0d310ee2132986525b896b9e08f85a18fe9
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 01:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const logDriverNames = function (driver, index, drivers) {
-  for (const name in driver) {
     console.log(driver.name);
-  }
 }
 
-
+[{name: "Annie"}, {name: "Bob"}].forEach(logDriverNames);
