58ce56ad91bfd8b0ee32d7c59839e80ae1cba049
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 03:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,11 +36,10 @@ function totalRevenue (drivers) {
 
 }
 
-totalRevenue(([
+totalRevenue([
   { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
   { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
   { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
   { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
   { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
 ]);
-)
