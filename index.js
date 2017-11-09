1b79073c47853cd399a89695ab9b0912c61b9fdc
Victoria Meng <nights1ststar@protonmail.com>
2017-11-09 01:50:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ function logDriverNames (drivers) {
   drivers.forEach(callback);
 }
 
-logDriverNames([{name: "annie"}, {name: "bobby"}])
+logDriverNames([
+  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+])
